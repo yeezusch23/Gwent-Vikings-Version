9aedec39c855ef416db07a4c74b27e9af50b3f9a
chaviano2004 <155854346+chaviano2004@users.noreply.github.com>
2024-04-18 16:42:26 -0400

    add Clima click

--- Assets/Scripts/Clima/ClimaClick.cs
+++ Assets/Scripts/Clima/ClimaClick.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClimaClick : MonoBehaviour
-{
+{   
+    [HideInInspector]
+    public GameObject controllerObject;
+    [HideInInspector]
+    public StartGame controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controllerObject = GameObject.Find("StartGame");
+        controller = controllerObject.GetComponent<StartGame>();
     }
 
-    void OnClimaClick()
+    public void OnClimaClick()
     {
-        
+        if (controller.selectedCard != null)
+        {
+            CardStats card = controller.selectedCard.transform.Find("Stats").GetComponent<CardStats>();
+             
+            if(card.row == "Clima")
+            {
+                controller.selectedCard.transform.GetComponent<CardHover>().cardActive = false;
+                controller.selectedCard.transform.GetComponent<CardSelect>().isSelectable = false;
+                controller.selectedCard.transform.SetParent(transform, false); 
+                controller.selectedCard = null;
+            }   
+            
+            controller.ResetField(1);
+            controller.ResetField(2);
+        }   
     }
 }
