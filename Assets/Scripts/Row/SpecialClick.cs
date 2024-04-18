4e6dcdb3fddb1fd61864aeb038422367c28dc6d0
chaviano2004 <155854346+chaviano2004@users.noreply.github.com>
2024-04-18 16:33:19 -0400

    add Special click

---
+++ Assets/Scripts/Row/SpecialClick.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialClick : MonoBehaviour
+{   
+    [HideInInspector]
+    public GameObject controllerObject;
+    [HideInInspector]
+    public StartGame controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerObject = GameObject.Find("StartGame");
+        controller = controllerObject.GetComponent<StartGame>();
+    }
+
+    public void OnSpecialClick()
+    {   
+        if (controller.selectedCard != null)
+        {
+            CardStats card = controller.selectedCard.transform.Find("Stats").GetComponent<CardStats>();
+            if((transform.parent.parent.name == "Player1" && card.faction == "Vikings") || (transform.parent.parent.name == "Player2" && card.faction == "Last Kingdom"))
+            {   
+                if(card.row == "Aumento" && transform.childCount == 0)
+                {
+                    controller.selectedCard.transform.GetComponent<CardHover>().cardActive = false;
+                    controller.selectedCard.transform.GetComponent<CardSelect>().isSelectable = false;
+                    controller.selectedCard.transform.SetParent(transform, false); 
+                    controller.selectedCard = null;
+                }   
+            }
+            controller.ResetField(1);
+            controller.ResetField(2);
+        }   
+    }
+}
